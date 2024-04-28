bdd5d4775eb008c8b6e23457f8a9da084874f30d
Sonyesplay <66852843+AndresMauricioBanguero@users.noreply.github.com>
2024-04-28 08:07:04 -0500

    Add files via upload

---
+++ djangoPagina1/config/db.py
@@ -0,0 +1,12 @@
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
+MYSQL = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'BaseP',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'PORT': '3306',
+    }
+}
